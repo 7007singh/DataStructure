89c84d4556562dfc4fbc00f6e8c7e6bd525b2651
7007singh <shw12021982@gmail.com>
2023-02-08 10:47:42 +0530

    [Shweta]Add: Adding the elements in linkedList by add last method

---
+++ DataStructure/CustomLinkedList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    internal class CustomLinkedList
+    {
+        public Node head;
+
+        public void AddLast(int data)
+        {
+            Node newNode = new Node(data);
+            if(head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = newNode;
+            }
+        }        
+
+        public void Display()
+        {
+            Console.WriteLine("Displaying nodes");
+            if(head == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+            }
+            else
+            {
+                Node temp = head;
+                while(temp != null)
+                {
+                    Console.Write(temp.data+" ");
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
---
+++ DataStructure/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CustomLinkedList linkedList = new CustomLinkedList();
+            linkedList.AddLast(56);
+            linkedList.AddLast(30);
+            linkedList.AddLast(70);
+            linkedList.Display();
+            Console.ReadLine();
+        }
+    }
+}
